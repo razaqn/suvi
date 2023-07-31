ed9efda23f0a94c8fd4b7d91fff6cfce53f15f29
Luthvv <lutfhi.r.g@gmail.com>
2023-07-31 10:54:52 +0700

    footer component

---
+++ suvi/resources/views/components/footer.blade.php
@@ -0,0 +1,38 @@
+ <div class="w-full px-24 bg-white">
+     <div class="w-full">
+         <div class="flex justify-between py-6">
+             <div class="flex gap-16">
+                 <a href="">About us</a>
+                 <a href="">Course</a>
+                 <a href="">Certification</a>
+                 <a href="">Achivement</a>
+             </div>
+
+             <div class="flex gap-10 items-center">
+                 <a href=""><img class="w-6 h-auto" src="{{ asset('logo/facebook-black.png') }}"
+                         alt=""></a>
+                 <a href=""><img class="w-6 h-auto" src="{{ asset('logo/twitter-black.png') }}"
+                         alt=""></a>
+                 <a href=""><img class="w-6 h-auto" src="{{ asset('logo/youtube-black.png') }}"
+                         alt=""></a>
+                 <a href=""><img class="w-7 h-auto" src="{{ asset('logo/instagram-black.png') }}"
+                         alt=""></a>
+             </div>
+         </div>
+
+         <div class="w-full h-[1px] bg-[#2B3D51]"></div>
+
+         <div class="flex items-center justify-between mt-7 text-sm mb-24">
+             <div>© 2019 Lift Media. All rights reserved.</div>
+
+             <div>
+                 <img class="w-1/2 m-auto" src="{{ asset('logo/suvi_logo.png') }}"alt="">
+             </div>
+
+             <div class="flex gap-8">
+                 <a href="">Terms of Service</a>
+                 <a href="">Privacy Policy</a>
+             </div>
+         </div>
+     </div>
+ </div>
