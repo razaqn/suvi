63de75256fc437386ed839597165c225afd8dc21
DimasLv07 <68526375+DimasLv07@users.noreply.github.com>
2023-08-09 13:10:40 +0700

    Update app.blade.php

--- suvi/resources/views/layouts/app.blade.php
+++ suvi/resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
