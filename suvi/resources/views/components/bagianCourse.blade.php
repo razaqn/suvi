c9c3ab300715d7687ec47a07f59fe45f413d5db7
DimasLv07 <68526375+DimasLv07@users.noreply.github.com>
2023-08-18 22:52:04 +0700

    Create bagianCourse.blade.php

---
+++ suvi/resources/views/components/bagianCourse.blade.php
@@ -0,0 +1,70 @@
+<div class="mb-[15px]">
+    <div class="flex my-[16px] justify-between">
+        <div>
+            <p class="text-base">Bagian 1: Pembukaan & Persiapan Bahan belajar</p>
+            <p class="text-sm mt-[9px]">3 video (10 menit)</p>
+        </div>
+        <svg id="buttonToggle{{ $id }}" xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 27 27"
+            fill="none">
+            <circle cx="13.5" cy="13.5" r="13" fill="#FF7E00" />
+            <path
+                d="M8.5 11.5L13.2308 16.3798C13.2653 16.4177 13.307 16.448 13.3533 16.4687C13.3996 16.4893 13.4495 16.5 13.5 16.5C13.5505 16.5 13.6004 16.4893 13.6467 16.4687C13.693 16.448 13.7347 16.4177 13.7692 16.3798L18.5 11.5"
+                stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    </div>
+    <div class="hidden" id="toggle{{ $id }}">
+        <div class="flex items-start mb-[17px] ">
+            @include('components.icon.checkmarkIcon')
+            <div class="ml-[7px]">
+
+                <p class="text-sm mb-[10px]">Pengenalan tools: laso tool</p>
+                <div class="flex items-center">
+                    @include('components.icon.tvIcon')
+                    <p class="text-xs ml-[4px]">10 Menit</p>
+
+
+                </div>
+            </div>
+        </div>
+        <div class="flex items-start mb-[17px]">
+            @include('components.icon.checkmarkIcon')
+            <div class="ml-[7px]">
+
+                <p class="text-sm mb-[10px]">Pengenalan tools: laso tool</p>
+                <div class="flex items-center">
+                    @include('components.icon.tvIcon')
+                    <p class="text-xs ml-[4px]">10 Menit</p>
+
+
+                </div>
+            </div>
+        </div>
+        <div class="flex items-start mb-[17px]">
+            @include('components.icon.checkmarkIcon')
+            <div class="ml-[7px]">
+
+                <p class="text-sm mb-[10px]">Pengenalan tools: laso tool</p>
+                <div class="flex items-center">
+                    @include('components.icon.tvIcon')
+                    <p class="text-xs ml-[4px]">10 Menit</p>
+
+
+                </div>
+            </div>
+        </div>
+        <div class="flex items-start mb-[17px]">
+            @include('components.icon.checkmarkIcon')
+            <div class="ml-[7px]">
+
+                <p class="text-sm mb-[10px]">Pengenalan tools: laso tool</p>
+                <div class="flex items-center">
+                    @include('components.icon.tvIcon')
+                    <p class="text-xs ml-[4px]">10 Menit</p>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
