0157dd0030d8f018257fa2288390ea7af43cf793
DimasLv07 <68526375+DimasLv07@users.noreply.github.com>
2023-08-20 04:16:27 +0700

    Update home.blade.php

--- suvi/resources/views/frontend/home.blade.php
+++ suvi/resources/views/frontend/home.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.front')
 
+
 @section('content')
-    <div class="mx-[30px] lg:mx-[136px]  mt-[136px] mt-[88px] mt-[136px]">
+    <div class="mx-[30px] lg:mx-[136px]   ">
         <div class="flex justify-between mb-[24px]">
             <h1 class=" text-[#1F1F1F] text-[20px] md:text-[25px] lg:text-[30px] font-[600] ops">Selamat Siang, Abdur Razaq</h1>
             <p class="text-[#FF7E00]  text-[14px] md:text-[18px] font-[400]">Kelas saya</p>
@@ -19,23 +20,28 @@
         </div>
 
     </div>
-    <div class="bg-[#FF7E00] mb-[64px] ">
+    <div style="z-index: -2" class="bg-[#FF7E00] mb-[64px] relative pb-[22px]">
+        <img style="z-index: -1" class="absolute my-[-36px] mx-[-80px] top-[37px] left-[0px] "
+            src="{{ asset('images/left-bub-main.png') }}" alt="">
+        <img style="z-index: -1" class="absolute my-[-36px] mx-[-80px] right-[80px] bottom-[35px]"
+            src="{{ asset('images/right-bub-main.png') }}" alt="">
         <div class="mx-[30px] lg:mx-[136px]  mt-[136px] mt-[88px]">
             <h1 class="ops text-[30px] font-[700] text-center text-white pt-[53px] mb-[32px]">Apa yang ingin anda pelajari
             </h1>
 
 
-            <div class="grid grid-cols-1  md:grid-cols-3 lg:grid-cols-4 gap-4 mt-[22px] justify-items-center pb-[56px]">
-                <div class="w-[172px] h-[172px] bg-white rounded-[5px]">
+            <div
+                class="grid grid-cols-1 gap-7 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4  mt-[22px]  pb-[56px] px-[20%]">
+                <div class="w-full h-[172px] mr-[10px] bg-white rounded-[5px]">
 
                 </div>
-                <div class="w-[172px] h-[172px] bg-white rounded-[5px]">
+                <div class="w-full mr-[100px] h-[172px] bg-white rounded-[5px]">
 
                 </div>
-                <div class="w-[172px] h-[172px] bg-white rounded-[5px]">
+                <div class="w-full mr-[10px] h-[172px] bg-white rounded-[5px]">
 
                 </div>
-                <div class="w-[172px] h-[172px] bg-white rounded-[5px]">
+                <div class="w-full mr-[10px] h-[172px] bg-white rounded-[5px]">
 
                 </div>
             </div>
@@ -50,22 +56,22 @@
         <hr class="mb-[16px]">
 
         <div class="grid grid-cols-1 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-1">
-            @include('components.productCard')
-            @include('components.productCard')
-            @include('components.productCard')
-            @include('components.productCard')
-            @include('components.productCard')
+            @include('components.verticalProductCard')
+            @include('components.verticalProductCard')
+            @include('components.verticalProductCard')
+            @include('components.verticalProductCard')
+            @include('components.verticalProductCard')
         </div>
 
         <h1 class="mb-[12px] text-[24px] text-[#1F1F1F] ops font-semibold">Direkomendasikan Untuk Anda </h1>
         <hr class="mb-[16px]">
 
         <div class="grid grid-cols-1 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-1">
-            @include('components.productCard')
-            @include('components.productCard')
-            @include('components.productCard')
-            @include('components.productCard')
-            @include('components.productCard')
+            @include('components.verticalProductCard')
+            @include('components.verticalProductCard')
+            @include('components.verticalProductCard')
+            @include('components.verticalProductCard')
+            @include('components.verticalProductCard')
 
         </div>
     </div>
