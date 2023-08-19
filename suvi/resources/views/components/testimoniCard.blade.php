caca337223eb975a75e7e65513744668a5684966
DimasLv07 <68526375+DimasLv07@users.noreply.github.com>
2023-08-20 02:28:21 +0700

    Update testimoniCard.blade.php

---
+++ suvi/resources/views/components/testimoniCard.blade.php
@@ -0,0 +1,22 @@
+<div class="mt-[20px] md:mt-[0px]">
+
+
+    <div class="flex justify-center mb-4">
+        @include('components.icon.starFillReview')
+        @include('components.icon.starFillReview')
+        @include('components.icon.starFillReview')
+        @include('components.icon.starFillReview')
+        @include('components.icon.startOutlineReview')
+    </div>
+    <p class="mb-[30px] text-center mts text-[14px] text-[#1F1F1F] w-[70%] m-auto">Kursus ini membuat saya
+        memiliki skill deisgn yang baik, dan juga kursus ini benar-benar mengajarkan saya dari 0, sangat
+        recomended!</p>
+    <div class="flex justify-center items-center">
+
+        <img class="rounded-full h-[50px] mr-[15px]" src="{{ asset('images/pp.png') }}" alt="">
+        <div>
+            <p class="text-[#FF7E00] text-[14px] font-bold mts">Regina Miles</p>
+            <p class="text-[#252B42] text-[12px] mts">Peserta kursus</p>
+        </div>
+    </div>
+</div>
