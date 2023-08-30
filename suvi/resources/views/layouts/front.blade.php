da7126261412b961a3e19e1fbffb1842f3f493ed
DimasLv07 <68526375+DimasLv07@users.noreply.github.com>
2023-08-30 14:45:17 +0700

    Update front.blade.php

--- suvi/resources/views/layouts/front.blade.php
+++ suvi/resources/views/layouts/front.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://cdn.tailwindcss.com"></script>
