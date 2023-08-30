82294ac3362860d6486d09591bba46fed0569bb7
DimasLv07 <68526375+DimasLv07@users.noreply.github.com>
2023-08-30 15:03:48 +0700

    Update front.blade.php

--- suvi/resources/views/layouts/front.blade.php
+++ suvi/resources/views/layouts/front.blade.php
@@ -14,7 +14,8 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-   
+   <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 </head>
 <body>
     <div id="app">
