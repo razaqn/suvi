c37944176786bcbab235a5f82bcfd84ccb0fbb02
DimasLv07 <68526375+DimasLv07@users.noreply.github.com>
2023-08-30 13:31:10 +0700

    Add files via upload

---
+++ suvi/resources/views/modal/landingPageModal.blade.php
@@ -0,0 +1,24 @@
+<div id="defaultModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+
+            <!-- Modal body -->
+            <div class="p-6 space-y-6">
+                <div class="bg-gray w-full bg-[#E5E5E5] h-[230px] rounded">
+
+                </div>
+                <h1 class="font-semibold text-[23px] text-[#1F1F1F]">Abdur Razaq Naufal</h1>
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                </p>
+            </div>
+            <!-- Modal footer -->
+            <div class="flex items-center p-6 space-x-2  rounded-b ">
+                <button data-modal-hide="defaultModal" type="button" class="text-white bg-[#FF7E00]  focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 w-full">Close</button>
+
+            </div>
+        </div>
+    </div>
+</div>
