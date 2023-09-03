05b63b7c35db985e80889d2e2d24de1d444b7553
DimasLv07 <68526375+DimasLv07@users.noreply.github.com>
2023-09-03 19:38:41 +0700

    Update front.blade.php

--- suvi/resources/views/layouts/front.blade.php
+++ suvi/resources/views/layouts/front.blade.php
@@ -26,5 +26,61 @@
     </div>
     @include('components.footer')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.0/flowbite.min.js"></script>
+
+    <script>
+                let defaultTransform = 0;
+
+                function goNext() {
+                    defaultTransform = defaultTransform - 398;
+                    var slider = document.getElementById("sliderPenghargaan");
+                    if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
+                    slider.style.transform = "translateX(" + defaultTransform + "px)";
+                }
+                next.addEventListener("click", goNext);
+
+                function goPrev() {
+                    var slider = document.getElementById("sliderPenghargaan");
+                    if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+                    else defaultTransform = defaultTransform + 398;
+                    slider.style.transform = "translateX(" + defaultTransform + "px)";
+                }
+                prev.addEventListener("click", goPrev);
+
+                let defaultTransform2 = 0;
+
+                function goNext2() {
+                    defaultTransform2 = defaultTransform2 - 398;
+                    var slider = document.getElementById("sliderPartner");
+                    if (Math.abs(defaultTransform2) >= slider.scrollWidth / 1.7) defaultTransform2 = 0;
+                    slider.style.transform = "translateX(" + defaultTransform2 + "px)";
+                }
+                next2.addEventListener("click", goNext2);
+
+                function goPrev2() {
+                    var slider = document.getElementById("sliderPartner");
+                    if (Math.abs(defaultTransform2) === 0) defaultTransform2 = 0;
+                    else defaultTransform2 = defaultTransform2 + 398;
+                    slider.style.transform = "translateX(" + defaultTransform2 + "px)";
+                }
+                prev2.addEventListener("click", goPrev2);
+
+                let defaultTransform3 = 0;
+
+                function goNext3() {
+                    defaultTransform3 = defaultTransform3 - 398;
+                    var slider = document.getElementById("sliderInstruktur");
+                    if (Math.abs(defaultTransform3) >= slider.scrollWidth / 1.7) defaultTransform3 = 0;
+                    slider.style.transform = "translateX(" + defaultTransform3 + "px)";
+                }
+                next3.addEventListener("click", goNext3);
+
+                function goPrev3() {
+                    var slider = document.getElementById("sliderInstruktur");
+                    if (Math.abs(defaultTransform3) === 0) defaultTransform3 = 0;
+                    else defaultTransform3 = defaultTransform3 + 398;
+                    slider.style.transform = "translateX(" + defaultTransform3 + "px)";
+                }
+                prev3.addEventListener("click", goPrev3);
+            </script>
 </body>
 </html>
